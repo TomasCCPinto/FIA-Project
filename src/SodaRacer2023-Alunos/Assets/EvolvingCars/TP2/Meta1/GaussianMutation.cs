cb8d66de088278015ead28a7970499a05ab8f38e
JoaoESmoreira <jesmoreira2010@gmail.com>
2023-04-15 14:50:00 +0100

    add all project

---
+++ src/SodaRacer2023-Alunos/Assets/EvolvingCars/TP2/Meta1/GaussianMutation.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Mutations;
+using GeneticSharp.Domain.Randomizations;
+using System;
+
+public class GaussianMutation : IMutation
+{
+    public bool IsOrdered { get; private set; } // indicating whether the operator is ordered (if can keep the chromosome order).
+    private double std = 1;
+
+    public GaussianMutation(double std)
+    {
+        IsOrdered = true;
+        this.std = std;
+
+    }
+
+    public GaussianMutation()
+    {
+        IsOrdered = true;
+
+    }
+
+    public void Mutate(IChromosome chromosome, float probability)
+    {
+        //YOUR CODE HERE
+
+    }
+
+    protected double SampleGaussian(double mean, double std)
+    {
+        //Generates a random number based on a normal distribution using the Box-Muller method.
+        double x1 = RandomizationProvider.Current.GetDouble(0, 1);
+        double x2 = RandomizationProvider.Current.GetDouble(0, 1);
+        double y1 = Math.Sqrt(-2.0 * Math.Log(x1)) * Math.Cos(2.0 * Math.PI * x2);
+        return y1 * std + mean;
+
+    }
+
+}
---
+++ src/SodaRacer2023-Alunos/Assets/EvolvingCars/TP2/Meta1/Roulette.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Populations;
+using GeneticSharp.Domain.Randomizations;
+using GeneticSharp.Domain.Selections;
+using GeneticSharp.Infrastructure.Framework.Texts;
+using GeneticSharp.Runner.UnityApp.Car;
+
+public class Roulette : SelectionBase
+{
+    public Roulette() : base(2)
+    {
+    }
+
+
+    
+  
+
+
+    protected override IList<IChromosome> PerformSelectChromosomes(int number, Generation generation)
+    {
+
+        IList<CarChromosome> population = generation.Chromosomes.Cast<CarChromosome>().ToList();
+        IList<IChromosome> parents = new List<IChromosome>();
+
+        //YOUR CODE HERE
+
+        return parents;
+    }
+}
